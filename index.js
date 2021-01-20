6c281b07e1e6b70c6b6839e8599267602715aa9e
JohnMohr <74803311+JohnMohr@users.noreply.github.com>
2021-01-19 22:16:52 -0800

    wip. expanding choices and their functions

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 const mysql = require('mysql');
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+const { start } = require('repl');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'employees_db',
+});
+
+
+connection.connect(function(err){
+    if (err) throw err;
+    start();
+});
+
+function start() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What do you want to do?",
+            choices: [
+                "View Employees",
+                "View Employees by Department",
+                "Exit",
+            ]
+        })
+        .then(function (answer){
+            switch (answer.action) {
+                case "View Employees":
+                    employee.viewAllEmployees(connection, start);
+                    break;
+                case "View Employees by Department":
+                    ViewEmployeeDept();
+                    break;
+
+                case "Exit":
+                    connection.end();
+                    break;
+
+            }
+        })
+}
